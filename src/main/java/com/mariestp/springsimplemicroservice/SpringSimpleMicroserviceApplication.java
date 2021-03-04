62363a6c5ce1915dd326f3482ec531793e7341f7
mariesto <sitinjakamendo@gmail.com>
2021-03-04 12:48:44 +0700

    Initialize Project

---
+++ src/main/java/com/mariestp/springsimplemicroservice/SpringSimpleMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mariestp.springsimplemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariestp/springsimplemicroservice/SpringSimpleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariestp.springsimplemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
